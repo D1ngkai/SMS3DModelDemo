81b8f074b11799dc50ab649a07913a4515a99135
lidingkai <lidingkai@ad.com>
2022-06-13 11:34:13 +0800

    Project Setup

---
+++ SMS3DModelDemo/SMS3DModelDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SMS3DModelDemo
+//
+//  Created by lidingkai on 2022/6/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SMS3DModelDemo/SMS3DModelDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SMS3DModelDemo
+//
+//  Created by lidingkai on 2022/6/13.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
